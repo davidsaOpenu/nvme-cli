f6f40b4d900945a3554d99cfc809d62def95b482
zzda <85033245+zzda@users.noreply.github.com>
2021-06-17 17:55:32 +0300

    Added object strategy
    A small change between the device driver and the nvme-cli contract regarding metadata.
    
    To be paired with #519094 and #519103
    
    Change-Id: I10b64caa31c2bc9531c1e8d6adb681fc4304b439

--- nvme-ioctl.h
+++ nvme-ioctl.h
@@ -21,7 +21,7 @@ int nvme_passthru(int fd, int ioctl_cmd, __u8 opcode, __u8 flags,
 /* NVME_SUBMIT_IO */
 int nvme_io(int fd, __u8 opcode, __u64 slba, __u16 nblocks, __u16 control,
 	      __u32 dsmgmt, __u32 reftag, __u16 apptag,
-	      __u16 appmask, void *data, void *metadata);
+	      __u16 appmask, void *data, void *metadata, __u16 metadata_len);
 
 int nvme_read(int fd, __u64 slba, __u16 nblocks, __u16 control,
 	      __u32 dsmgmt, __u32 reftag, __u16 apptag,
