46083a531344c33ecafb90cc0978142fe971527b
Nataniel Liberman <nataniel521@gmail.com>
2023-05-02 12:51:20 -0400

    Added object commands read, write, list and delete from cli side.
    Also added data size calculation, struct definition for nvme_user_obj_io,
    size of length of read data is acquired through the driver writing it back to
    the io struct.
    
    -Switched offset and data-size to be 64-bit
    -Cleaned up command specific handling and error messages
    -Fixed bug where the wrong arg was used as obj_id
    -Read and list now acquire data in chunks, looping until less than the buffer size is acquired.
    -Removed command specific error messages to be in line with regular block commands.
    -Added dirent to upai
    -objl parsing now uses dirent
    -Default to using obj_id as data file name unless it is passed using -data
    
    Trello card: https://trello.com/c/brOq3vV9/87-set-up-evssim-env-object-read-write-list-delete-support-in-nvme-cli
    
    Change-Id: I76cff730070d788dccf405f69114f13add8a9c88

--- linux/nvme_ioctl.h
+++ linux/nvme_ioctl.h
@@ -32,6 +32,21 @@ struct nvme_user_io {
 	__u16	appmask;
 };
 
+#define NVME_OBJ_ID_MAXLEN 64
+
+struct nvme_user_obj_io {
+	__u8	opcode;
+	__u64   offset;
+	__u64	length; // in/out
+	__u64	addr;
+	__u8	obj_id[NVME_OBJ_ID_MAXLEN];
+};
+
+struct nvme_user_obj_dirent {
+	__u8	obj_id[NVME_OBJ_ID_MAXLEN];
+	__u64	obj_len;
+};
+
 struct nvme_passthru_cmd {
 	__u8	opcode;
 	__u8	flags;
@@ -62,5 +77,6 @@ struct nvme_passthru_cmd {
 #define NVME_IOCTL_RESET	_IO('N', 0x44)
 #define NVME_IOCTL_SUBSYS_RESET	_IO('N', 0x45)
 #define NVME_IOCTL_RESCAN	_IO('N', 0x46)
+#define NVME_IOCTL_SUBMIT_OBJ_IO	_IOWR('N', 0x47, struct nvme_user_obj_io)
 
 #endif /* _UAPI_LINUX_NVME_IOCTL_H */
